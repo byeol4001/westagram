fb5f5a900ff35414f441aa52bd004585471014bb
이한별 <maleebook@MaLeeBook.local>
2020-06-04 15:48:18 +0900

    css

--- js/main.js
+++ js/main.js
@@ -2,25 +2,22 @@ const commentValue = document.getElementById("comments_input");
 const commentButton = document.getElementById("comments_button");
 const commentsList = document.getElementsByClassName("comments_list")[0];
 
-
-
-commentButton.addEventListener('click', function() {
+function change(){
     var li = document.createElement('li');
     var span = document.createElement('span');
     commentsList.appendChild(li);
-    li.innerHTML = `<span><span class="comments_name">WECODE</span>${commentValue.value}</span>`
+    li.innerHTML = `<span><span class="comments_name">byeol21_</span>${commentValue.value}</span>`
     li.className = "comments_list_li";
     commentValue.value = '';
+}
+
+commentButton.addEventListener('click', function() {
+    change();
 })
 
 
 commentValue.addEventListener('keypress', function(event) {
     if (event.keyCode === 13) {
-        var li = document.createElement('li');
-        var span = document.createElement('span');
-        commentsList.appendChild(li);
-        li.innerHTML = `<span><span class="comments_name">WECODE</span>${commentValue.value}</span>`
-        li.className = "comments_list_li";
-        commentValue.value = '';
+        change();
     }
   });
