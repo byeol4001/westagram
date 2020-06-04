e97ebd84f51d2cb23cb5da8820649a7e965b7f13
이한별 <maleebook@MaLeeBook.local>
2020-06-04 11:48:44 +0900

    modify

--- js/login.js
+++ js/login.js
@@ -1,14 +1,6 @@
-
-// const loginInput = document.querySelector('#input');
 const idInput = document.getElementById('idinput');
 const pwInput = document.getElementById('pwinput');
 const loginButton = document.querySelector('button');
-
-
-// loginInput.addEventListener('keypress', function() {
-//   loginButton.style.backgroundColor = '#0094f6'
-// })
-
 
 
 pwInput.addEventListener('keypress', function() {
@@ -18,19 +10,3 @@ pwInput.addEventListener('keypress', function() {
 })
 
 
-
-// loginInput.addEventListener('input', function() {
-//         loginButton.style.backgroundColor = '#0094f6'
-//   });
-
-  // loginButton.addEventListener('click', function() {
-  //   // if(!idTyping === !pwTyping === false){
-  //   //     loginButton.style.backgroundColor = '#0094f6'
-  //   // }else{
-  //   //     loginButton.style.backgroundColor = '#0094f69f'
-  //   // }
-  //   if(!pwTyping.value === true){
-  //       loginButton.style.backgroundColor = '#0094f6'
-  //     }
-
-  // });
--- js/main.js
+++ js/main.js
@@ -7,8 +7,6 @@ const commentsList = document.getElementsByClassName("comments_list")[0];
 commentButton.addEventListener('click', function() {
     var li = document.createElement('li');
     var span = document.createElement('span');
-    // var text = document.createTextNode(commentValue.value);
-    // li.appendChild(text);
     commentsList.appendChild(li);
     li.innerHTML = `<span><span class="comments_name">WECODE</span>${commentValue.value}</span>`
     li.className = "comments_list_li";
@@ -20,8 +18,6 @@ commentValue.addEventListener('keypress', function(event) {
     if (event.keyCode === 13) {
         var li = document.createElement('li');
         var span = document.createElement('span');
-        //var text = document.createTextNode(commentValue.value);
-        //li.appendChild(text);
         commentsList.appendChild(li);
         li.innerHTML = `<span><span class="comments_name">WECODE</span>${commentValue.value}</span>`
         li.className = "comments_list_li";
